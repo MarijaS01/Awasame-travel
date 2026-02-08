e53be64e6deb804930acedf809fa34ee602ce53b
MarijaS01 <marija.shanina@gmail.com>
2026-02-08 20:04:37 +0200

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: 'Awasame Travel',
+  description: 'Your next journey starts here',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="de">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
